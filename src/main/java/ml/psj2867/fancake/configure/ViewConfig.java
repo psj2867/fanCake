c2e3b58fd9481214add9639371c17be9db4bb678
psj2867 <psj2867@gmail.com>
2021-10-28 10:16:22 +0000

    cors preflight 처리 때문에 was단에서 설정

--- src/main/java/ml/psj2867/fancake/configure/ViewConfig.java
+++ src/main/java/ml/psj2867/fancake/configure/ViewConfig.java
@@ -23,4 +23,13 @@ public class ViewConfig implements WebMvcConfigurer{
         bean.setValidationMessageSource(messageSource);
         return bean;
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")            
+                .maxAge(3600)
+                ;
+    }
 }
