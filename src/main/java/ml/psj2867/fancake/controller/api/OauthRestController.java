087392a60ca1a85e38c0fae3dffe67ab1e42890a
psj2867 <psj2867@gmail.com>
2021-11-06 05:25:59 +0000

    api 설명 추가

--- src/main/java/ml/psj2867/fancake/controller/api/OauthRestController.java
+++ src/main/java/ml/psj2867/fancake/controller/api/OauthRestController.java
@@ -3,16 +3,16 @@ package ml.psj2867.fancake.controller.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import io.swagger.annotations.Api;
 import ml.psj2867.fancake.service.user.AuthService;
-import ml.psj2867.fancake.service.user.model.auth.NaverOAuthForm;
 import ml.psj2867.fancake.service.user.model.auth.NaverToken;
 
+@Api(description="sns 로그인 관련")
 @Controller
 @RequestMapping("api/oauth")
 public class OauthRestController {
@@ -22,13 +22,8 @@ public class OauthRestController {
 
     @ResponseBody
     @PostMapping("naver") 
-    public String naverCallback(@RequestBody @Validated NaverToken naverToken){
+    public String postNaverAccessToken(@RequestBody @Validated NaverToken naverToken){
         return authService.loginNaverUserOrAdd(naverToken).getAccessToken();
-    }
-
-    @GetMapping("naver_test") 
-    public String naverTest(NaverOAuthForm naverOAuthForm){
-        return "redirect:https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=PdW0nmtpfhryHj5UrU_C&redirect_uri=http%3A%2F%2Fdev2.psj2867.com%3A8080%2Fapi%2Foauth%2Fnaver&state=897364419183268226652408826015092631592";
     }
 
 }
--- src/main/java/ml/psj2867/fancake/controller/api/UsersRestController.java
+++ src/main/java/ml/psj2867/fancake/controller/api/UsersRestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import ml.psj2867.fancake.configure.security.TokenDto;
@@ -30,6 +31,8 @@ import ml.psj2867.fancake.service.user.model.UserListForm;
 import ml.psj2867.fancake.service.user.model.UserLoginForm;
 import ml.psj2867.fancake.service.user.model.sign.SignUpUserForm;
 import ml.psj2867.fancake.util.MessageDto;
+
+@Api(basePath = "api/users", description="사용자 정보")
 @RestController
 @RequestMapping("api/users")
 public class UsersRestController {
