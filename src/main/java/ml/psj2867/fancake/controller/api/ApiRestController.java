8a6c10e2f3cb7d0ed0b21871082750ba6f91bc42
psj2867 <psj2867@gmail.com>
2021-11-24 07:30:14 +0000

    stomp sample page

--- src/main/java/ml/psj2867/fancake/controller/api/ApiRestController.java
+++ src/main/java/ml/psj2867/fancake/controller/api/ApiRestController.java
@@ -2,19 +2,26 @@ package ml.psj2867.fancake.controller.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import ml.psj2867.fancake.util.MessageDto;
 
 
-@RestController
+@Controller
 @RequestMapping("api")
 public class ApiRestController {
 
+    @ResponseBody
     @RequestMapping("*")
     public ResponseEntity<Object> notFound(){  
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(MessageDto.of("Unmapped Url"));
     }
 
+    @GetMapping("stomp")
+    public String stompTestStatic(){
+        return "forward:/static/stomp.html";
+    }
 }
