702861edcbdae20e8c1e865ed6fe6bbb76db4596
psj <psj2867@gmail.com>
2021-12-11 01:16:48 +0900

    ws 에 필요없는 인증 제거

--- src/main/java/ml/psj2867/fancake/configure/WebSocketConfig.java
+++ src/main/java/ml/psj2867/fancake/configure/WebSocketConfig.java
@@ -1,13 +1,10 @@
 package ml.psj2867.fancake.configure;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-import ml.psj2867.fancake.configure.security.WebSocketFilter;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -24,10 +21,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 		.setAllowedOriginPatterns("*")
 		.withSockJS();
 	}
-	@Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(new WebSocketFilter());
-    }
 
 }
 
